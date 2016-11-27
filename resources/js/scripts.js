1ae66e8c121412624a441ea26e08ada26e092d63
lvmvr <lvmvr.x@gmail.com>
2016-11-26 22:55:36 -0500

    Add uncompleted tasks section.

---
+++ resources/js/scripts.js
@@ -0,0 +1,7 @@
+// User will click on the 'Add' button
+document.getElementById('add').addEventListener('click', function() {
+  var val = document.getElementById('item').value;
+    if (val) { // If there is indeed a value input...
+      
+    } 
+});
